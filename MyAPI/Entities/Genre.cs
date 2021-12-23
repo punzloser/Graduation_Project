33eae16d9f1ca4e7e67d07f870e63768c91d6431
Thanh Nguyen <punzloser@gmail.com>
2021-12-23 22:44:27 +0700

    Test Custom Validation

--- MyAPI/Entities/Genre.cs
+++ MyAPI/Entities/Genre.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyAPI.Validations;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -9,7 +10,8 @@ namespace MyAPI.Entities
     public class Genre
     {
         public int Id { get; set; }
-        [Required(ErrorMessage = "Nhập thể loại")]
+        //[Required(ErrorMessage = "Nhập thể loại")]
+        [GenreAttribute]
         public string Name { get; set; }
     }
 }

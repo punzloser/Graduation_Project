8b0870419b70805b31d7a5a339dfe3e15942def4
Thanh Nguyen <punzloser@gmail.com>
2021-12-25 17:24:51 +0700

    Thiết đặt CLI Framework Core

---
+++ MyAPI/MyDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyAPI.Entities;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyAPI
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext([NotNullAttribute] DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Genre> Genres { get; set; }
+    }
+}
--- MyAPI/Startup.cs
+++ MyAPI/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -29,6 +30,11 @@ namespace MyAPI
         {
 
             services.AddControllers();
+
+            services.AddDbContext<MyDbContext>(opt =>
+            {
+                opt.UseSqlServer(Configuration.GetConnectionString("Default"));
+            });
 
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer();
 

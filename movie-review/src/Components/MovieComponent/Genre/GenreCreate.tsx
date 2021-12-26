cb3030c567485631421d46460d6d06de6bd4d403
Thanh Nguyen <punzloser@gmail.com>
2021-12-26 23:40:16 +0700

    Hiển thị lỗi validate phía Api

--- movie-review/src/Components/MovieComponent/Genre/GenreCreate.tsx
+++ movie-review/src/Components/MovieComponent/Genre/GenreCreate.tsx
@@ -1,26 +1,32 @@
 import axios from "axios";
+import { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { genreUrl } from "../../../endpoints";
+import { DisplayErrors } from "../../Utilities/DisplayErrors";
 import { defaultGenre } from "./defaultGenre";
 import { GenreForm } from "./GenreForm";
 import { IGenre } from './IGenre'
 
 export const GenreCreate = () => {
 
+    const [errs, setErrs] = useState<string[]>([]);
     const history = useHistory();
 
     const create = async (genre: IGenre) => {
         try {
             await axios.post(genreUrl, genre);
             history.push('/the-loai')
-        } catch (error) {
-            console.log(error)
+        } catch (error: any) {
+            if (error || error.response) {
+                setErrs(error.response.data);
+            }
         }
     }
 
     return (
         <div className="container-fluid">
             <h3 className="text-muted">Khởi tạo</h3>
+            <DisplayErrors errors={errs} />
             <GenreForm
                 initValue={defaultGenre}
                 onSubmit={async e => {

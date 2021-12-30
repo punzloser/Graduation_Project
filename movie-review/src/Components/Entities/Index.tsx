fbe4cace495dab145efee125388e9ceb359d75c9
Thanh Nguyen <punzloser@gmail.com>
2021-12-30 23:39:55 +0700

    Component dùng chung : Index

---
+++ movie-review/src/Components/Entities/Index.tsx
@@ -0,0 +1,89 @@
+import { useEffect, useState } from "react";
+import axios, { AxiosResponse } from 'axios';
+import { Link } from "react-router-dom";
+import { Btn } from "../Utilities/Btn";
+import { CustomConfirm } from "../Utilities/CustomConfirm";
+import { Pagination } from "../Utilities/Pagination";
+import { RecordsFilter } from "../Utilities/RecordsFilter";
+import { GeneralList } from "../Utilities/GeneralList";
+import { ReactElement } from "react-markdown/lib/react-markdown";
+
+interface IIndex<T> {
+    urlBase: string,
+    urlCreate: string,
+    title: string,
+    children: (entityList: T[], buttons: (urlUpdate: string, id: number) => ReactElement) => ReactElement
+}
+
+export default function Index<T>(props: IIndex<T>) {
+    const [entityList, setEntityList] = useState<T[]>([]);
+    const [recordsPerPage, setRecordsPerPage] = useState(5);
+    const [totalOfPages, setTotalOfPages] = useState(0);
+    const [page, setPage] = useState(1);
+
+    const indexLoading = () => {
+
+        axios.get(props.urlBase, {
+            params: { page, recordsPerPage }
+        })
+            .then((response: AxiosResponse<T[]>) => {
+                const totalOfRecords = parseInt(response.headers['totalofrecords'], 10);
+                setTotalOfPages(Math.ceil(totalOfRecords / recordsPerPage));
+                setEntityList(response.data);
+            })
+    }
+
+    useEffect(() => {
+        indexLoading();
+        // eslint-disable-next-line react-hooks/exhaustive-deps 
+    }, [page, recordsPerPage])
+
+    const del = async (id: number) => {
+        try {
+            await axios.delete(`${props.urlBase}?id=${id}`)
+            indexLoading();
+
+        } catch (error: any) {
+            if (error || error.response) {
+                console.log(error.response.data)
+            }
+        }
+    }
+
+    const buttons = (urlUpdate: string, id: number) => (
+        <>
+            <Link className="btn btn-lg btn-warning" to={urlUpdate}>Sửa đổi</Link>
+            <Btn
+                bgColor="btn btn-lg btn-danger ms-2"
+                onClick={() => CustomConfirm(() => del(id))}
+            >
+                Xóa
+            </Btn>
+        </>
+    );
+
+    return (
+        <div className="container-fluid">
+            <h3 className="text-muted">{props.title}</h3>
+            <Link className="btn btn-lg btn-primary" to={props.urlCreate}>Khởi tạo</Link>
+            <Pagination currentPage={page} totalOfPages={totalOfPages} onChange={e => setPage(e)} />
+            <RecordsFilter onChange={(e) => {
+                setPage(1);
+                setRecordsPerPage(e);
+            }} />
+
+            <GeneralList _list={entityList}>
+                <table className="table table-hover table-responsive table-striped w-75 mb-5">
+                    <thead>
+                        <tr>
+                            <th>{props.title}</th>
+                            <th>Chức năng</th>
+                        </tr>
+                    </thead>
+
+                    {props.children(entityList!, buttons)}
+                </table>
+            </GeneralList>
+        </div>
+    );
+}
--- movie-review/src/Components/MovieComponent/Genre/GenreIndex.tsx
+++ movie-review/src/Components/MovieComponent/Genre/GenreIndex.tsx
@@ -1,88 +1,32 @@
-import axios, { AxiosResponse } from "axios";
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import { genreUrl } from "../../../endpoints";
-import { Btn } from "../../Utilities/Btn";
-import { CustomConfirm } from "../../Utilities/CustomConfirm";
-import { GeneralList } from "../../Utilities/GeneralList";
-import { Pagination } from "../../Utilities/Pagination";
-import { RecordsFilter } from "../../Utilities/RecordsFilter";
+import Index from "../../Entities/Index";
 import { genreDTO } from './IGenre';
 
 export const GenreIndex = () => {
-
-    const [genreList, setGenreList] = useState<genreDTO[]>([]);
-    const [recordsPerPage, setRecordsPerPage] = useState(5);
-    const [totalOfPages, setTotalOfPages] = useState(0);
-    const [page, setPage] = useState(1);
-
-    const indexLoading = () => {
-
-        axios.get(genreUrl, {
-            params: { page, recordsPerPage }
-        })
-            .then((response: AxiosResponse<genreDTO[]>) => {
-                const totalOfRecords = parseInt(response.headers['totalofrecords'], 10);
-                setTotalOfPages(Math.ceil(totalOfRecords / recordsPerPage));
-                setGenreList(response.data);
-            })
-    }
-
-    useEffect(() => {
-        indexLoading();
-        // eslint-disable-next-line react-hooks/exhaustive-deps 
-    }, [page, recordsPerPage])
-
-    const del = async (id: number) => {
-        try {
-            await axios.delete(`${genreUrl}?id=${id}`)
-            indexLoading();
-
-        } catch (error: any) {
-            if (error || error.response) {
-                console.log(error.response.data)
-            }
-        }
-    }
-
     return (
-        <div className="container-fluid">
-            <h3 className="text-muted">Thể loại</h3>
-            <Link className="btn btn-lg btn-primary" to="/the-loai/them">Khởi tạo</Link>
-            <Pagination currentPage={page} totalOfPages={totalOfPages} onChange={e => setPage(e)} />
-            <RecordsFilter onChange={(e) => {
-                setPage(1);
-                setRecordsPerPage(e);
-            }} />
-            <GeneralList _list={genreList}>
-                <table className="table table-hover table-responsive table-striped w-75 mb-5">
-                    <thead>
-                        <tr>
-                            <th>Thể loại</th>
-                            <th>Chức năng</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            genreList?.map((e, i) =>
-                                <tr key={i}>
-                                    <td>{e.name}</td>
-                                    <td>
-                                        <Link className="btn btn-lg btn-warning" to={`the-loai/sua/${e.id}`}>Sửa đổi</Link>
-                                        <Btn
-                                            bgColor="btn btn-lg btn-danger ms-2"
-                                            onClick={() => CustomConfirm(() => del(e.id))}
-                                        >
-                                            Xóa
-                                        </Btn>
-                                    </td>
-                                </tr>
-                            )
-                        }
-                    </tbody>
-                </table>
-
-            </GeneralList>
-        </div>
+        <>
+            <Index<genreDTO>
+                urlBase={genreUrl}
+                urlCreate="the-loai/them"
+                title="Thể loại"
+            >
+                {(genres, buttons) => (
+                    <>
+                        <tbody>
+                            {
+                                genres?.map((e, i) =>
+                                    <tr key={i}>
+                                        <td>{e.name}</td>
+                                        <td>
+                                            {buttons(`the-loai/sua/${e.id}`, e.id)}
+                                        </td>
+                                    </tr>
+                                )
+                            }
+                        </tbody>
+                    </>
+                )}
+            </Index>
+        </>
     );
 }

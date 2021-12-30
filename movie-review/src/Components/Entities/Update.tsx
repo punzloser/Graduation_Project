917200bc77a26c7267e7c667e80d76d023f8b1fe
Thanh Nguyen <punzloser@gmail.com>
2021-12-30 18:47:47 +0700

    Component dùng chung : Update

---
+++ movie-review/src/Components/Entities/Update.tsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+import { useHistory, useParams } from "react-router-dom";
+import axios, { AxiosResponse } from "axios";
+import { DisplayErrors } from "../Utilities/DisplayErrors";
+import { Loading } from "../Utilities/Loading";
+import { ReactElement } from "react-markdown/lib/react-markdown";
+
+interface IUpdate<TCreation, TRead> {
+    urlBase: string,
+    urlIndex: string,
+    transform(entity: TRead): TCreation,
+    children(entity: TCreation, update: (entity: TCreation) => void): ReactElement
+}
+
+export default function Update<TCreation, TRead>(props: IUpdate<TCreation, TRead>) {
+
+    const history = useHistory();
+    const { id }: any = useParams();
+    const [entity, setEntity] = useState<TCreation>();
+    const [errs, setErrs] = useState<string[]>([]);
+
+    const update = async (model: TCreation) => {
+        try {
+            await axios.put(`${props.urlBase}/${id}`, model);
+            history.push(props.urlIndex);
+        } catch (err: any) {
+            if (err || err.response) {
+                setErrs(err.response.data);
+            }
+        }
+    };
+
+    useEffect(() => {
+        axios.get(`${props.urlBase}/${id}`)
+            .then((response: AxiosResponse<TRead>) => {
+                setEntity(props.transform(response.data));
+            });
+    }, [id]);
+    
+    return (
+        <div className="container-fluid">
+            <h3 className="text-muted">Sửa đổi</h3>
+            <DisplayErrors errors={errs} />
+            {!entity ? <Loading /> : props.children(entity, update)}
+        </div>
+    );
+}
+
+Update.defaultProps = {
+    transform: (entity: any) => entity
+}
--- movie-review/src/Components/MovieComponent/Genre/GenreUpdate.tsx
+++ movie-review/src/Components/MovieComponent/Genre/GenreUpdate.tsx
@@ -1,49 +1,24 @@
-import axios, { AxiosResponse } from "axios";
-import { useEffect, useState } from "react";
-import { useHistory, useParams } from "react-router-dom";
 import { genreUrl } from "../../../endpoints";
-import { DisplayErrors } from "../../Utilities/DisplayErrors";
-import { Loading } from "../../Utilities/Loading";
+import Update from "../../Entities/Update";
 import { GenreForm } from "./GenreForm";
-import { IGenre } from "./IGenre";
+import { genreDTO, IGenre } from "./IGenre";
 
 export const GenreUpdate = () => {
-    const history = useHistory();
-    const { id }: any = useParams();
-    const [genre, setGenre] = useState<IGenre>();
-    const [errs, setErrs] = useState<string[]>([]);
 
-    const update = async (genre: IGenre) => {
-        try {
-            await axios.put(`${genreUrl}/${id}`, genre);
-            history.push('/the-loai');
-        } catch (err: any) {
-            if (err || err.response) {
-                setErrs(err.response.data);
-            }
-        }
-    }
-
-    useEffect(() => {
-        axios.get(`${genreUrl}/${id}`)
-            .then((response: AxiosResponse<IGenre>) => {
-                setGenre(response.data);
-            })
-    }, [id]);
     return (
-        <div className="container-fluid">
-            <h3 className="text-muted">Sửa đổi</h3>
-            <DisplayErrors errors={errs} />
-            {
-                !genre ? <Loading /> :
-
+        <>
+            <Update<IGenre, genreDTO>
+                urlBase={genreUrl}
+                urlIndex="/the-loai"
+            >
+                {(entity, update) =>
                     <GenreForm
-                        initValue={genre}
-                        onSubmit={async e => {
+                        initValue={entity}
+                        onSubmit={async (e) => {
                             await update(e);
-                        }}
-                    />
-            }
-        </div>
+                        }} />
+                }
+            </Update>
+        </>
     );
 }
